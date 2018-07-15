4ff82d2cc249e3d3b6e351896f962eb19f12615b
tc200828731 <200828731@qq.com>
2018-07-15 19:53:00 +0800

    快排 及 归并优化

--- src/learn/baseAlgs/PrimarySort.java
+++ src/learn/baseAlgs/PrimarySort.java
@@ -1,6 +1,7 @@
 package learn.baseAlgs;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.io.File;
@@ -72,46 +73,158 @@ public class PrimarySort {
     public static void mergeUpDownSort(Comparable[] a){
         int len = a.length;
         aux = new Comparable[len];
-        mergeUpDownSort( a, 0, len-1);
+        mergeUpDownSort(a, 0, len-1);
     }
     private static void mergeUpDownSort(Comparable[] a, int lo, int hi){
         if(hi<=lo) return;
-        int mid = lo + (hi-lo)/2;
+        int mid = lo + (hi - lo)/2;
         mergeUpDownSort(a, lo, mid);
         mergeUpDownSort(a, mid+1, hi);
         merge(a, lo, mid, hi);
     }
     /* 归并排序中用于归并的方法 先复制到辅助数组再归并 */
     private static void merge(Comparable[] a, int lo, int mid, int hi){
-        for(int k = lo; k <= hi; k++){
-            aux[k] = a[k];
-        }
-        int i = lo;
-        int j = mid+1;
-        for(int k = lo; k <= hi;k++){
-            if(i>mid) a[k] = aux[j++];
-            else if(j>hi) a[k] = aux[i++];
-            else if(less(aux[j],aux[i])) a[k] = aux[j++];
-            else a[k] = aux[i++];
-        }
+       for(int k = lo; k < a.length; k++){
+           aux[k] = a[k];
+       }
+       int i = lo;
+       int j = mid+1;
+       for(int k = lo; k < a.length; k++){
+           if(i>mid) a[k] = aux[j++];
+           else if(j>hi) a[k] = aux[i++];
+           else if(less(aux[i],aux[j])) a[k] = aux[i++];
+           else a[k] = aux[j++];
+       }
     }
     /**
      * 归并排序 - 自底向上
      * 先归并微型数组再成对归并
      * */
+    private static Comparable[] aux2;
     public static void mergeDownUpSort(Comparable[] a){
         int len = a.length;
-        aux = new Comparable[len];
+        aux2 = new Comparable[len];
         for(int sz = 1; sz < len; sz= sz+sz){
             for(int lo = 0; lo < len-sz; lo +=sz+sz){
-                merge(a, lo, lo+sz-1, Math.min(lo+sz+sz-1, len-1));
+                mergeDU(a, lo, lo+sz-1, Math.min(lo+sz+sz-1, len-1));
             }
+        }
+    }
+    /* 归并排序中用于归并的方法 先复制到辅助数组再归并 */
+    private static void mergeDU(Comparable[] a, int lo, int mid, int hi){
+        for(int k = lo; k < a.length; k++){
+            aux2[k] = a[k];
+        }
+        int i = lo;
+        int j = mid+1;
+        for(int k = lo; k < a.length; k++){
+            if(i>mid) a[k] = aux2[j++];
+            else if(j>hi) a[k] = aux2[i++];
+            else if(less(aux2[i],aux2[j])) a[k] = aux2[i++];
+            else a[k] = aux2[j++];
+        }
+    }
+    /**
+     * 改进的归并排序
+     * 1.长度短的数组直接使用插入排序(例如长度小于15时)
+     * 2.测试数组有序来跳过无用归并
+     * 3.节省元素复制到辅助数组的时间
+     * */
+    public static void mergeSortOptimize(Comparable[] a){
+        int len = a.length;
+        Comparable[] b = a.clone();
+        mergeSortOptimize(a, b,0, len-1);
+    }
+    public static void mergeSortOptimize(Comparable[] a,Comparable[] b,int lo,int hi){
+        if(lo>=hi) return;
+        int mid = lo + (hi-lo)/2;
+        if(hi-lo<=15){
+            //长度短的数组直接使用插入排序(例如长度小于15时)
+            insertionSort(a);
+            return;
+        }
+        mergeSortOptimize(b,a,lo,mid);
+        mergeSortOptimize(b,a,mid+1,hi);
+        if(less(a[mid],a[mid+1])){
+            //测试数组有序来跳过无用归并
+            mergeSO(a,b,lo,mid,hi);
+        }
+    }
+    private static void mergeSO(Comparable[] a, Comparable[] b,int lo, int mid, int hi){
+        int i = lo;
+        int j = mid+1;
+        for(int k = lo; k < a.length; k++){
+            if(i>mid) a[k] = b[j++];
+            else if(j>hi) a[k] = b[i++];
+            else if(less(b[i],b[j])) a[k] = b[i++];
+            else a[k] = b[j++];
         }
     }
 
     /**
-     * 改进的归并排序
+     * 快速排序
+     * 按大小切分数组，每次切分排定一个元素
+     * */
+    public static void quickSort(Comparable[] a){
+        StdRandom.shuffle(a);
+        quickSort(a,0,a.length-1);
+    }
+    public static void quickSort(Comparable[] a, int lo, int hi){
+        if(hi<=lo){
+            return;
+        }
+        int j = partition(a,lo,hi);
+        quickSort(a,lo,j-1);
+        quickSort(a,j+1,hi);
+    }
+
+    /*快速排序的切分*/
+    private static int partition(Comparable[] a, int lo, int hi){
+        int key = lo;
+        int i = lo;
+        int j = hi+1;
+        while (true){
+            while(less(a[++i],a[key])) if(i==hi) break;
+            while(less(a[key],a[--j])) if(j==lo) break;
+            if(i>=j) break;
+            exch(a, i, j);
+        }
+        exch(a,j,lo);
+        return j;
+    }
+/*    public static void quickSort(Comparable[] a, int lo, int hi){
+        if(lo>=hi){
+            return;
+        }
+        int key = lo;
+        int i = lo;
+        int j = hi;
+        while (i<j){
+            while(i<a.length&&(less(a[i],a[key]) || a[i]==a[key])){
+                i++;
+            }
+            while(less(a[key],a[j])){
+                j--;
+            }
+            if(i<j) {
+                exch(a, i, j);
+            }
+        }
+        int mid = Math.min(i,j);
+        exch(a,key,mid);
+        quickSort(a,lo,mid-1);
+        quickSort(a,mid+1,hi);
+    }*/
+
+    /**
+     * 改进的快速排序
      * 1.长度短的数组直接使用插入排序
+     * 2.三取样
+     * 3.针对大量重复数组*/
+
+
+    /**
+     * 堆排序
      * */
 
 
@@ -154,7 +267,7 @@ public class PrimarySort {
         long a1 = System.currentTimeMillis();
         System.out.println(a1-start);
         String[] c = a.clone();
-        mergeDownUpSort(c);
+        quickSort(c);
         show(c);
         long a2 = System.currentTimeMillis();
         System.out.println(a2-a1);
